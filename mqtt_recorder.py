b443a98f2e6e0e9a34fad0e93832b32199b961c9
Jakob Schlyter <jakob@kirei.se>
2025-04-23 11:28:05 +0200

    Update and use aiomqtt

--- mqtt_recorder.py
+++ mqtt_recorder.py
@@ -5,87 +5,79 @@ import asyncio
 import base64
 import json
 import logging
-import os
-import signal
 import sys
 import time
 
-from asyncio_mqtt import Client
-
-logger = logging.getLogger("mqtt_recorder")
+import aiomqtt
 
 
-async def mqtt_record(client: Client, output: str = None, topic: str = "#") -> None:
+async def mqtt_record(
+    hostname: str,
+    output_filename: str | None = None,
+    topic: str = "#",
+) -> None:
     """Record MQTT messages"""
-    if output is not None:
-        output_file = open(output, "wt")
-    else:
-        output_file = sys.stdout
-    await client.connect()
-    await client.subscribe(topic)
-    async with client.unfiltered_messages() as messages:
-        async for message in messages:
-            record = {
-                "time": time.time(),
-                "qos": message.qos,
-                "retain": message.retain,
-                "topic": message.topic,
-                "msg_b64": base64.urlsafe_b64encode(message.payload).decode(),
-            }
-            logger.info("Received: %s", record)
-            print(json.dumps(record), file=output_file)
-            output_file.flush()
+
+    with open(output_filename, "w") if output_filename else sys.stdout as output_file:
+        async with aiomqtt.Client(hostname) as client:
+            await client.subscribe(topic)
+            async for message in client.messages:
+                record: dict[str, str | float] = {
+                    "time": time.time(),
+                    "qos": message.qos,
+                    "retain": message.retain,
+                    "topic": str(message.topic),
+                    "msg_b64": base64.urlsafe_b64encode(message.payload).decode(),  # type: ignore
+                }
+                logging.info("Received: %s", json.dumps(record))
+                print(json.dumps(record), file=output_file)
+                output_file.flush()
 
 
 async def mqtt_replay(
-    client: Client,
-    input: str = None,
+    hostname: str,
+    input_filename: str | None = None,
     delay: int = 0,
     realtime: bool = False,
     scale: float = 1,
 ) -> None:
     """Replay MQTT messages"""
-    await client.connect()
-    if input is not None:
-        input_file = open(input, "rt")
-    else:
-        input_file = sys.stdin
-    if delay > 0:
-        static_delay_s = delay / 1000
-    else:
-        static_delay_s = 0
-    last_timestamp = None
-    for line in input_file:
-        record = json.loads(line)
-        logger.info("%s", record)
-        if "msg_b64" in record:
-            msg = base64.urlsafe_b64decode(record["msg_b64"].encode())
-        elif "msg" in record:
-            msg = record["msg"].encode()
-        else:
-            logger.warning("Missing message attribute: %s", record)
-            next
-        logger.info("Publish: %s", record)
-        await client.publish(
-            record["topic"],
-            msg,
-            retain=record.get("retain"),
-            qos=record.get("qos", 0),
-        )
-        delay_s = static_delay_s
-        if realtime or scale != 1:
-            delay_s += (
-                record["time"] - last_timestamp if last_timestamp else 0
-            ) * scale
-            last_timestamp = record["time"]
-        if delay_s > 0:
-            logger.debug("Sleeping %.3f seconds", delay_s)
-            await asyncio.sleep(delay_s)
 
+    with open(input_filename) if input_filename else sys.stdin as input_file:
+        async with aiomqtt.Client(hostname) as client:
+            static_delay_s = delay / 1000 if delay > 0 else 0
+            last_timestamp = None
 
-async def shutdown(sig, loop):
-    loop.stop()
-    os._exit(-1)
+            for line in input_file:
+                record = json.loads(line)
+                logging.debug("%s", record)
+
+                if "msg_b64" in record:
+                    msg = base64.urlsafe_b64decode(record["msg_b64"].encode())
+                elif "msg" in record:
+                    msg = record["msg"].encode()
+                else:
+                    logging.warning("Missing message attribute: %s", record)
+                    continue
+
+                logging.info("Publish: %s", record)
+                await client.publish(
+                    record["topic"],
+                    msg,
+                    retain=record.get("retain"),
+                    qos=record.get("qos", 0),
+                )
+
+                delay_s = static_delay_s
+
+                if realtime or scale != 1:
+                    delay_s += (
+                        record["time"] - last_timestamp if last_timestamp else 0
+                    ) * scale
+                    last_timestamp = record["time"]
+                if delay_s > 0:
+                    logging.debug("Sleeping %.3f seconds", delay_s)
+                    await asyncio.sleep(delay_s)
 
 
 def main():
@@ -144,24 +136,21 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    client = Client(hostname=args.server)
-
     if args.mode == "replay":
         process = mqtt_replay(
-            client=client,
-            input=args.input,
+            hostname=args.server,
+            input_filename=args.input,
             delay=args.delay,
             realtime=args.realtime,
             scale=1 / args.speed,
         )
     else:
-        process = mqtt_record(client=client, output=args.output)
+        process = mqtt_record(
+            hostname=args.server,
+            output_filename=args.output,
+        )
 
-    loop = asyncio.get_event_loop()
-    for s in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(s, lambda: asyncio.ensure_future(shutdown(s, loop)))
-
-    loop.run_until_complete(process)
+    asyncio.run(process)
 
 
 if __name__ == "__main__":
